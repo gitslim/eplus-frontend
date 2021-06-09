feef51f011e07d2e97380d4d513917199c5ea0aa
Jecjvorobey <jeckvorobey69@gmail.com>
2021-06-10 00:41:44 +0300

    сделал страницу комплексные решения и вынес отдельным компонентом CallbackForm

---
+++ src/components/callbackForm.svelte
@@ -0,0 +1,58 @@
+<script>
+</script>
+
+<style lang="scss">
+  .contact-link {
+    background: #f86923;
+    color: #fff;
+    padding: 30px;
+  }
+
+  .title__bottom {
+    font-size: 16px;
+    font-weight: 700;
+    margin-bottom: 10px;
+  }
+
+  .btn-white {
+    color: #222;
+    margin-top: 20px;
+    border: 2px solid#fff;
+    background: #fff;
+    border-radius: 30px;
+    overflow: hidden;
+    transition: all 0.3s;
+    font-size: 12px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    padding: 9px 24px 9px;
+    position: relative;
+    display: inline-block;
+    z-index: 1;
+    font-weight: 700;
+
+    &:hover {
+      color: #fff;
+      background-color: #f86923;
+    }
+  }
+
+</style>
+
+<div class="content block-content">
+  <div class="contact-link">
+    <div class="title__bottom">Нужна консультация ?</div>
+    <div class="desc">
+      Позвоните нам по телефону:<br />
+      <b style="font-size:15px;" class="roistat-phone">+7 (495) 790-76-97</b><br />
+      или воспользуйтесь формой обратной связи
+    </div>
+    <div class="action">
+      <!--<a class="btn-white" href="#/modal/contact-form">обратная связь</a>-->
+      <a
+        class="btn-white"
+        data-dialog-type="modal"
+        href="/contact/kontaktnaa_forma">Мы перезвоним вам</a>
+    </div>
+  </div>
+</div>
--- src/routes/kompleksnye-resheniya.svelte
+++ src/routes/kompleksnye-resheniya.svelte
@@ -1,1 +1,71 @@
-Комплексные решения
+<script>
+  import CallbackForm from "../components/callbackForm.svelte";
+
+</script>
+
+<style lang="scss">
+  .column-content {
+    padding-left: 15px;
+  }
+  
+  .content__wrap {
+    display: flex
+  }
+
+  ul {
+    list-style: disc;
+  }
+  .content-block {
+    width: 65%;
+    margin-right: 10px;
+  }
+  .content-bottom {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+  .sidebar-right {
+    margin: 0 auto;
+  }
+
+</style>
+
+<main class="container">
+  <div class="row content__wrap">
+    <div class="content-block col-8">
+      <section>
+        <div class="widget">
+          <h2 class="title">
+            <span><span>Комплексные решения</span> </span><span
+              class="heading-line" />
+          </h2>
+        </div>
+        <div class="column-content">
+          <ul>
+            <li>
+              <a href="/tehniceskoe-perevooruzenie">Техническое перевооружение
+                котельной</a>
+            </li>
+            <li>
+              <a
+                href="/vnutriplosadocnye-i-vnutrennie-gazoprovody">Внутриплощадочные
+                и внутренние газопроводы</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <div class="content-bottom">
+        <CallbackForm />
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 sidebar sidebar-right theiaStickySidebar">
+			<div class="sidebar-inner">
+        <div class="content block-content">
+            <div class="field field--name-body field--type-text-with-summary field--label-hidden field__item">
+              <h3 style="color:#ff6600; font-size:19px;">ВИДЕОПРЕЗЕНТАЦИЯ КОМПАНИИ</h3>
+              <p><iframe allowfullscreen="" frameborder="0" height="208" src="https://www.youtube.com/embed/L8tZf_xNUWQ" width="370"></iframe></p>
+          </div>   
+        </div>  
+      </div>
+    </div>
+  </div>
+</main>
