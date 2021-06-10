de4338d866343138338fb97241bb1cc1bbab68af
Jecjvorobey <jeckvorobey69@gmail.com>
2021-06-10 09:38:28 +0300

    нес отдельным компонентом правый сайдбар

--- src/routes/kompleksnye-resheniya.svelte
+++ src/routes/kompleksnye-resheniya.svelte
@@ -1,5 +1,6 @@
 <script>
-  import CallbackForm from "../components/callbackForm.svelte";
+import CallbackForm from "../components/callbackForm.svelte";
+import SidebarRight from "../components/sidebarRight.svelte";
 
 </script>
 
@@ -7,7 +8,7 @@
   .column-content {
     padding-left: 15px;
   }
-  
+
   .content__wrap {
     display: flex
   }
@@ -23,8 +24,12 @@
     margin-top: 20px;
     margin-bottom: 20px;
   }
-  .sidebar-right {
-    margin: 0 auto;
+
+  .block-sidebar {
+    margin: 10px;
+    margin-left: auto;
+    margin-right: auto;
+
   }
 
 </style>
@@ -57,15 +62,8 @@
         <CallbackForm />
       </div>
     </div>
-    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 sidebar sidebar-right theiaStickySidebar">
-			<div class="sidebar-inner">
-        <div class="content block-content">
-            <div class="field field--name-body field--type-text-with-summary field--label-hidden field__item">
-              <h3 style="color:#ff6600; font-size:19px;">ВИДЕОПРЕЗЕНТАЦИЯ КОМПАНИИ</h3>
-              <p><iframe allowfullscreen="" frameborder="0" height="208" src="https://www.youtube.com/embed/L8tZf_xNUWQ" width="370"></iframe></p>
-          </div>   
-        </div>  
-      </div>
+    <div class="block-sidebar">
+      <SidebarRight/>
     </div>
   </div>
 </main>
