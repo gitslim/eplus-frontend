ebe7d84187ed0f26348b28bea5d6760f27ce7711
Jecjvorobey <jeckvorobey69@gmail.com>
2021-06-11 14:26:53 +0300

    переименовал коратинки, сделал несколько доп компонентов

---
+++ src/components/PurposeCard.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let src = "",
+    alt = "",
+    text = "";
+
+</script>
+
+<style lang="scss">
+  .target {
+    box-shadow: 0 19px 46px 3px rgb(0 0 0 / 15%);
+    border-radius: 5px;
+    min-height: 450px;
+    background-color: #fff;
+    margin-bottom: 30px;
+
+    &__img {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 200px;
+      border-bottom: 2px dashed #3838387a;
+    }
+
+    &__info {
+      padding: 35px 22px;
+      font-size: 16px;
+      font-weight: 300;
+      line-height: 30px;
+    }
+  }
+
+  img {
+    border: 0;
+    vertical-align: top;
+    max-width: 100%;
+    height: auto;
+  }
+
+</style>
+
+<div class="target">
+  <div class="target__img"><img {src} {alt} /></div>
+
+  <div class="target__info">
+    <p>{text}</p>
+  </div>
+</div>
---
+++ src/components/TitleLanding.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let titleText = "";
+
+</script>
+
+<style lang="scss">
+  .title {
+    margin-bottom: 40px;
+    position: relative;
+    text-transform: uppercase;
+    font-size: 30px;
+
+    &::before {
+      content: "";
+      display: block;
+      position: absolute;
+      width: 100px;
+      height: 5px;
+      top: -20px;
+      left: 50%;
+      transform: translateX(-50%);
+      background-color: #f60;
+    }
+  }
+
+</style>
+
+<h2 class="title">{titleText}</h2>
