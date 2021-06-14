2d50f35f18337d0aea609bb0cd5390f329e82ee8
Jecjvorobey <jeckvorobey69@gmail.com>
2021-06-14 09:32:46 +0300

    fix page

---
+++ src/routes/page/tehnicheskoe-perevooruzhenie-kotelnoj.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  import SvelteSeo from "svelte-seo";
+
+</script>
+
+<script>
+  import HeaderLanding from "../../components/HeaderLanding.svelte";
+  import TitleLanding from "../../components/TitleLanding.svelte";
+  import PurposeCard from "../../components/PurposeCard.svelte";
+
+  let title = "Техническое переворужение котельной";
+
+</script>
+
+<style lang="scss">
+  /* your styles go here */
+
+</style>
+
+<p>Техническое переворужение котельной</p>
