07cd26216b586e81a5b4a8ec35ee48c81f11fc81
Jecjvorobey <jeckvorobey69@gmail.com>
2021-06-15 12:57:25 +0300

    обавил компонент nputForm, промежуточные сохранения

--- src/components/GaslinesForm.svelte
+++ src/components/GaslinesForm.svelte
@@ -1,9 +1,84 @@
 <script>
   // your script goes here
 
+  import InputForm from "./InputForm.svelte";
+
 </script>
 
-<style>
-  /* your styles go here */
+<style lang="scss">
+  .form {
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+    &__group {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+
+      &__subtext {
+        margin-bottom: 20px;
+        color: #fff;
+      }
+    }
+  }
+
+  .input__wrap {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: left;
+    width: 360px;
+  }
+
+  hr {
+    width: 100%;
+    height: 2px;
+    background-color: gray;
+  }
 
 </style>
+
+<form class="form">
+  <div class="form__group">
+    <div class="input__wrap">
+      <div class="form__subtext">Вид работ</div>
+      <label for="projectOnSiteGas">
+        <input type="checkbox" id="projectOnSiteGas" name="projectOnSiteGas" />
+        проект на внутриплощадочный газопровод</label>
+      <label for="projecItnternalGas">
+        <input
+          type="checkbox"
+          id="projecItnternalGas"
+          name="projecItnternalGas" />
+        проект на внутренний газопровод</label>
+      <label for="installationOnSiteGas">
+        <input
+          type="checkbox"
+          id="installationOnSiteGas"
+          name="installationOnSiteGas" />
+        монтаж внутриплощадочного газопровода</label>
+      <label for="installationInternalGas">
+        <input
+          type="checkbox"
+          id="installationInternalGas"
+          name="installationInternalGas" />
+        монтаж внутреннего газопровода</label>
+    </div>
+    <hr />
+  </div>
+  <div class="container is-max-widescreen computing">
+    <InputForm id="name" name="name" placeholder="Ваше Имя" />
+
+    <!-- <label for="phoneOrEmail">Телефон или Email</label>
+      <input
+        type="text"
+        id="phoneOrEmail"
+        name="phoneOrEmail"
+        placeholder="Телефон или Email" />
+
+    <label for="cost"> <input type="text" id="cost" name="cost" /> </label> -->
+  </div>
+</form>
--- src/routes/page/vnutriploshadochnye-gazoprovody.svelte
+++ src/routes/page/vnutriploshadochnye-gazoprovody.svelte
@@ -8,7 +8,6 @@
   import HeaderLanding from "../../components/HeaderLanding.svelte";
   import InfoBlock from "../../components/InfoBlock.svelte";
   import TitleLanding from "../../components/TitleLanding.svelte";
-  import PurposeCard from "../../components/PurposeCard.svelte";
   import CalculationBlock from "../../components/CalculationBlock.svelte";
 
   let title = "Внутреплощадочные и внутренние газопроводы";
@@ -22,6 +21,33 @@
     subText:
       "С утверждением Правил подключения (технологического присоединения) объектов капитального строительства к газораспределительным сетям (Постановления РФ от 30.12.13 № 1314) следует особо отметить, что мероприятия по подключению в пределах границ земельного участка осуществляются заявителем (заказчиком-застройщиком), а мероприятия по подключению до границы земельного участка осуществляются исполнителем (газораспределительной организацией).",
   };
+
+  const functionsCard = [
+    {
+      id: 1,
+      text: "Внутриплощадочный газопровод - участок распределительного газопровода (ввод), находящийся внутри производственной территории предприятия, обеспечивающий подачу газа к потребителю».",
+      alt: "image-card",
+      src: "/images/gazoprovodi/connecting-event__icon1.png",
+    },
+    {
+      id: 2,
+      text: "Внутренний газопровод - газопровод, проложенный внутри здания от вводного (внутриплощадочного) газопровода до места установки газоиспользующего оборудования..",
+      alt: "image-card",
+      src: "images/gazoprovodi/connecting-event__icon2.png",
+    },
+    {
+      id: 3,
+      text: "ПРГ (пункт редуцирования газа) - технологическое устройство сетей газораспределения и газопотребления, предназначенное для снижения давления газа и поддержания его в заданных пределах независимо от расхода газа.",
+      alt: "image-card",
+      src: "images/gazoprovodi/connecting-event__icon3.png",
+    },
+    {
+      id: 4,
+      text: "Узел учета газа - функционально объединенная совокупность средств измерительной техники, которая преобразует выходной сигнал средства измерений объема и расхода газа при рабочих условиях, измеряет все или некоторые необходимые параметры потока и среды и вычисляет объем и расход газа, приведенные к стандартным условиям.",
+      alt: "image-card",
+      src: "/images/gazoprovodi/connecting-event__icon4.png",
+    },
+  ];
 
 </script>
 
@@ -49,6 +75,51 @@
   .functions {
     padding-top: 80px;
 
+    &__title {
+      padding: 0 15px;
+    }
+    &__targets {
+      display: flex;
+
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
+    &__target {
+      display: flex;
+      width: 45%;
+      min-height: 255px;
+      box-shadow: 0 19px 46px 3px rgba(0, 0, 0, 0.15);
+      border-radius: 5px;
+      background-color: #fff;
+      margin-bottom: 30px;
+
+      &-img {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 200px;
+        min-height: 255px;
+        min-width: 160px;
+        &img {
+          border: 0;
+          vertical-align: top;
+          max-width: 100%;
+          height: auto;
+        }
+      }
+
+      &-info {
+        display: flex;
+        align-items: center;
+        padding: 0 50px;
+        border-left: 2px dashed #3838387a;
+        font-size: 14px;
+        color: rgb(56, 56, 56);
+        line-height: 1.286;
+        text-align: left;
+      }
+    }
+
     &__desc {
       max-width: 893px;
       margin: 0 auto 50px;
@@ -58,28 +129,32 @@
     }
 
     &__help {
+      text-align: center;
     }
     &__icon {
       padding-top: 10px;
+      margin-bottom: 20px;
       position: relative;
       display: flex;
       justify-content: center;
     }
 
     &__step {
-      //width: 17%;
-      // display: flex;
-      // justify-content: center;
-      // flex-direction: column;
+      text-align: center;
       padding: 0 15px;
+      position: relative;
+      width: 21%;
+
+      &:last-child::after {
+        display: none;
+      }
 
       &::before {
         content: "";
         display: block;
         position: absolute;
-        top: 20px;
+        top: 93px;
         left: 50%;
-        -webkit-transform: translateX(-50%);
         transform: translateX(-50%);
         width: 16px;
         height: 16px;
@@ -91,11 +166,14 @@
       &::after {
         content: "";
         position: absolute;
-        left: 20px;
-        top: 20px;
-        height: 100%;
+        left: 245px;
+        top: -13px;
+        height: 122%;
         width: 2px;
         border-right: 2px dashed #3838387a;
+        -webkit-transform: rotate(90deg);
+        -ms-transform: rotate(90deg);
+        transform: rotate(90deg);
       }
       &-img {
         width: 56px;
@@ -110,6 +188,14 @@
         position: relative;
       }
     }
+  }
+
+  .calculation {
+    background-image: url("/images/tehnicheskoePerevooruzhenie/form-bg.jpg");
+    text-align: center;
+    color: #fff;
+    padding: 100px 0 70px;
+    background-size: cover;
   }
 
 </style>
@@ -131,9 +217,12 @@
   </div>
 
   <section class="section functions">
-    <div class="container is-widescreen">
-      <TitleLanding
-        titleText="Что включают в себя мероприятия по подключению в пределах границ земельного участка для заявителя?" />
+    <div class="container is-max-widescreen">
+      <div class="functions__title">
+        <TitleLanding
+          titleText="Что включают в себя мероприятия по подключению в пределах границ земельного участка для заявителя?" />
+      </div>
+
       <p class="functions__desc">
         Мероприятия подразумевают проектирование и строительство заявителем
         внутриплощадочных газопроводов, средств антикоррозионной защиты,
@@ -142,27 +231,40 @@
         регулирования процесса сжигания газа.
       </p>
 
+      <div class="functions__targets columns is-desktop">
+        {#each functionsCard as item}
+          <div class="functions__target">
+            <div class="functions__target-img">
+              <img src={item.src} alt={item.alt} />
+            </div>
+            <div class="functions__target-info">
+              <p>{item.text}</p>
+            </div>
+          </div>
+        {/each}
+      </div>
+
       <div class="functions__help">
         <p class="functions__desc">
           Наряду с оперативным выполнением работ, мы окажем всестороннюю
           экспертную поддержку:
         </p>
-        <div class="functions__icon is-half">
-          <div class="functions__step is-1">
+        <div class="functions__icon columns">
+          <div class="functions__step column-gap is-2">
             <img
               src="/images/gazoprovodi/events-help__icon1.png"
               alt="help-icon"
               class="functions__step-img" />
             <p class="functions__step-text">Техническое сопровождение</p>
           </div>
-          <div class="functions__step is-one-fifth">
+          <div class="functions__step column-gap is-2">
             <img
               src="/images/gazoprovodi/events-help__icon2.png"
               alt="help-icon"
               class="functions__step-img" />
             <p class="functions__step-text">Юридическое сопровождение</p>
           </div>
-          <div class="functions__step is-1">
+          <div class="functions__step column-gap is-2">
             <img
               src="/images/gazoprovodi/events-help__icon3.png"
               alt="help-icon"
@@ -171,6 +273,11 @@
           </div>
         </div>
       </div>
+    </div>
+  </section>
+  <section class="section calculation">
+    <div class="container is-max-widescreen">
+      <CalculationBlock />
     </div>
   </section>
 </main>
