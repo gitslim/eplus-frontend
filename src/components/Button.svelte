c89e7002b96a5f9f455ae2e41f2f85cdf8fae231
Jecjvorobey <jeckvorobey69@gmail.com>
2021-06-17 20:30:56 +0300

    сделал форму Газопроводы, посел обрботчики, создал компонент Button

---
+++ src/components/Button.svelte
@@ -0,0 +1,37 @@
+<script>
+  function clickHandler(e) {
+    console.log("click ===>", e.target);
+  }
+
+</script>
+
+<style lang="scss">
+  .btn {
+    width: 100%;
+    color: #222;
+    margin-top: 20px;
+    border: 2px solid #f86923;
+    background: #f86923;
+    // border-radius: 30px;
+    overflow: hidden;
+    -webkit-transition: all 0.3s;
+    -o-transition: all 0.3s;
+    transition: all 0.3s;
+    font-size: 12px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    padding: 9px 24px 9px;
+    position: relative;
+    display: inline-block;
+    font-weight: 700;
+
+    &:hover {
+      background: #fff;
+    }
+  }
+
+</style>
+
+<button class="btn" on:click|preventDefault={clickHandler}>
+  <slot />
+</button>
