c776a2170b1d33f6c30e01b215b1184b5f2644e3
Jecjvorobey <jeckvorobey69@gmail.com>
2021-07-01 00:21:57 +0300

    add couter yandex

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -109,6 +109,58 @@
       </div>
     </div>
   </div>
+  <div class="counter columns">
+    <div class="column is-3 is-offset-3">
+      <!-- Yandex.Metrika counter -->
+      <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+          m[i] =
+            m[i] ||
+            function () {
+              (m[i].a = m[i].a || []).push(arguments);
+            };
+          m[i].l = 1 * new Date();
+          (k = e.createElement(t)),
+            (a = e.getElementsByTagName(t)[0]),
+            (k.async = 1),
+            (k.src = r),
+            a.parentNode.insertBefore(k, a);
+        })(
+          window,
+          document,
+          "script",
+          "https://mc.yandex.ru/metrika/tag.js",
+          "ym"
+        );
+
+        ym(54841009, "init", {
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true,
+          webvisor: true,
+        });
+      </script><noscript><div>
+          <img
+            src="https://mc.yandex.ru/watch/54841009"
+            style="position:absolute; left:-9999px;"
+            alt="" />
+        </div></noscript>
+      <!-- /Yandex.Metrika counter -->
+      <script>
+        (function (w, d, u) {
+          var s = d.createElement("script");
+          s.async = true;
+          s.src = u + "?" + ((Date.now() / 60000) | 0);
+          var h = d.getElementsByTagName("script")[0];
+          h.parentNode.insertBefore(s, h);
+        })(
+          window,
+          document,
+          "https://cdn.bitrix24.ru/b9447275/crm/site_button/loader_2_ux0fep.js"
+        );
+      </script>
+    </div>
+  </div>
 
   <!--modal blok -->
   <Modal>
