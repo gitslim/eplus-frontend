8b126820c85b3b242529493729a06980435ff804
wdr83 <91600121+wdr83@users.noreply.github.com>
2021-11-09 21:30:44 +0300

    Лэндинг газопрод и ГРС дополнение карусель и этапы

--- src/routes/page/proektirovanie-magistralnyh-gazoprovodov-i-grs.svelte
+++ src/routes/page/proektirovanie-magistralnyh-gazoprovodov-i-grs.svelte
@@ -4,9 +4,9 @@
 
 
 <script>
-    import CalculationBlock from '$lib/components/CalculationBlock.svelte'
     import {onMount} from 'svelte'
     import {browser} from '$app/env'
+    import {Lightbox, LightboxImage, LightboxGallery} from 'svelte-lightbox'
     import BtnZayavka from '$lib/components/BtnZayavka.svelte'
     import ContactFormButton from '$lib/components/ContactFormButton.svelte'
     import ContactForm from '$lib/components/ContactForm.svelte'
@@ -23,6 +23,29 @@
         for(var i = 0; i < buttonraunded.length; i++) {
             buttonraunded[i].style.borderRadius = '30px';
         }
+
+        let right = document.getElementsByClassName('right');
+        for(var i = 0; i < right.length; i++) {
+            right[i].style.borderRadius = '50%';
+            right[i].style.backgroundColor = '#f86923';
+            right[i].style.right = '2vw';
+            right[i].style.color = '#ffffff';
+            right[i].style.cursor = 'pointer';
+        }
+        if (document.documentElement.clientWidth < 500) {
+            right[2].style.top = '-3%';
+        }
+
+
+        let left = document.getElementsByClassName('left');
+        for(var i = 0; i < left.length; i++) {
+            left[i].style.borderRadius = '50%';
+            left[i].style.backgroundColor = '#f86923';
+            left[i].style.left = '';
+            left[i].style.right = '6vw';
+            left[i].style.color = '#ffffff';
+            left[i].style.cursor = 'pointer';
+        }      
 
     })
 
@@ -154,6 +177,129 @@
         display: none;
     }
 
+    .logic__item.svelte-rgyqg6 {
+        position: relative
+    }
+
+    .logic__item.svelte-rgyqg6:last-child::after {
+        display: none
+    }
+
+    .logic__item.svelte-rgyqg6::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 96px;
+        left: 50%;
+        -webkit-transform: translateX(-50%);
+        -ms-transform: translateX(-50%);
+        transform: translateX(-50%);
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        background: #fff;
+        border: 4px solid #f60
+    }
+
+    .logic__item.svelte-rgyqg6::after {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 101px;
+        left: 52%;
+        width: 110%;
+        height: 4px;
+        border-bottom: 2px dashed #383838;
+        opacity: 0.5
+    }
+   
+    .logic__item.svelte-rgyqg6.rgyqg7::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 96px;
+        left: 50%;
+        -webkit-transform: translateX(-50%);
+        -ms-transform: translateX(-50%);
+        transform: translateX(-50%);
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        background: #fff;
+        border: 4px solid #f60
+    }
+
+    .logic__item.svelte-rgyqg6.rgyqg7::after {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 101px;
+        left: 51%;
+        width: 108%;
+        height: 4px;
+        border-bottom: 2px dashed #383838;
+        opacity: 0.5
+    }
+
+    /**************************/
+    /*Стиль текста карусели НАШИ ПРОЕКТЫ - start*/
+    .project {
+        background: #ededed;
+        margin: 50px 0px;
+    }
+
+    .image .item-content-2 {
+        top: 50%;
+        left: 0px;
+        right: 0px;
+        z-index: 2;
+        padding: 0px 30px;
+        text-align: center;
+        position: absolute;
+        transform: translateY(-50%);
+    }
+
+    .image .item-content-2 .item-title {
+        opacity: 0;
+        width: 100%;
+        display: block;
+        // color: white;
+        color:#ffffff;
+        text-shadow: 2px 0 2px #f95d10, 
+                     0 2px 2px #f95d10, 
+                     -2px 0 2px #f95d10, 
+                     0 -2px 2px #f95d10;       
+        line-height: 1.4;
+        transform: translateY(8px);
+        transition: all 0.3s ease-in-out;
+        font-size: 21px;
+        font-weight: 600;
+        margin-bottom: 10px;
+    }
+
+    // .image .item-content-2 .item-title {
+    //     font-size: 21px;
+    //     font-weight: 600;
+    //     margin-bottom: 10px;
+    // }
+
+    .image:hover .item-content-2 .item-title {
+        opacity: 1;
+        transform: translateY(0px);
+        transition-delay: 0.3s;
+    }
+
+    // .image:hover .item-content-2 .item-title {
+    //     transition-delay: 0.3s;
+    // }
+
+    // .proect-carousel-item {
+    //     padding: 0 10px;
+    // }
+
+    /*Стиль текста карусели НАШИ ПРОЕКТЫ - end*/
+    /*******************************/
+
 
     /**************************************/
     /*Экран меньше 1220px START*/
@@ -163,6 +309,81 @@
         }
     }
     /*Экран меньше 1220px END*/
+    /**************************************/
+
+     /**************************************/
+    /*Экран меньше 1023px START*/
+
+    @media screen and (max-width: 1023px) {
+        .logic__icon.svelte-rgyqg6 {
+            height: 56px;
+        }
+
+        .logic__item.svelte-rgyqg6 {
+            position: relative
+        }
+
+        .logic__item.svelte-rgyqg6:last-child::after {
+            display: block;
+        }
+
+        .logic__item.svelte-rgyqg6::before {
+            content: "";
+            display: block;
+            position: absolute;
+            top: 96px;
+            left: 50%;
+            -webkit-transform: translateX(-50%);
+            -ms-transform: translateX(-50%);
+            transform: translateX(-50%);
+            width: 16px;
+            height: 16px;
+            border-radius: 50%;
+            background: #fff;
+            border: 4px solid #f60
+        }
+
+        .logic__item.svelte-rgyqg6::after {
+            content: "";
+            display: block;
+            position: absolute;
+            top: 101px;
+            left: 20%;
+            width: 60%;
+            height: 4px;
+            border-bottom: 2px dashed #383838;
+            opacity: 0.5
+        }
+
+        .logic__item.svelte-rgyqg6.rgyqg7::before {
+            content: "";
+            display: block;
+            position: absolute;
+            top: 96px;
+            left: 50%;
+            -webkit-transform: translateX(-50%);
+            -ms-transform: translateX(-50%);
+            transform: translateX(-50%);
+            width: 16px;
+            height: 16px;
+            border-radius: 50%;
+            background: #fff;
+            border: 4px solid #f60
+        }
+
+        .logic__item.svelte-rgyqg6.rgyqg7::after {
+            content: "";
+            display: block;
+            position: absolute;
+            top: 101px;
+            left: 20%;
+            width: 60%;
+            height: 4px;
+            border-bottom: 2px dashed #383838;
+            opacity: 0.5
+        }
+    }
+        /*Экран меньше 1023px START*/
     /**************************************/
 
     /**************************************/
@@ -213,6 +434,7 @@
         .zayvka {
             padding-top: 40px;
         }
+      
     }
 
     /*Экран меньше 769px STOP*/
@@ -224,7 +446,7 @@
 <SvelteSeo {title}/>
 
 <main>
-
+    
     <section class="hero is-medium hero1">
         <div class="hero-body">
             <div class="columns">
@@ -247,6 +469,7 @@
         </div>
     </section>
 
+    
     <section class="section">
         <div class="container">
             <div class="content">               
@@ -391,6 +614,194 @@
             </div>           
         </div>
     </section>
+
+    <style>
+        .proect-carousel-item {
+            max-heigt: 200px;
+        }
+    </style>
+
+    <section class="section py-6 project">
+        <div class="container text-center">
+
+            <div class="title titlefor has-text-centered ">
+                <h2 class="">Наши проекты</h2>
+            </div>
+
+            <Carousel perPage={{1000:4, 800:3, 500:2, 400:1}}
+                      dots={false}
+                      loop={false}>
+
+                        
+                <span class="control" slot="left-control">
+                            <Icon icon={faChevronLeft}/>
+                </span>
+
+                <div class="slide-content proect-carousel-item">
+                    <figure class="image"> 
+                        <span class="item-content-2">
+                            <strong class="item-title">Проект 1</strong>
+                        </span>
+                    <Lightbox   noScroll="false"
+                                modalClasses = "pipu"
+                                thumbnail>
+                        <img slot="thumbnail" src="/images/gazoprovodi/project-1t.png" alt="Thumbnail">
+                        <img slot="image" src="/images/gazoprovodi/project-1.png" alt="Lightbox image">
+                    </Lightbox>
+                </figure>
+                </div>
+
+                <div class="slide-content proect-carousel-item">
+                    <figure class="image"> 
+                        <span class="item-content-2">
+                            <strong class="item-title">Проект 2</strong>
+                        </span>
+                    <Lightbox   noScroll="false"
+                                modalClasses = "pipu"
+                                thumbnail>
+                        <img slot="thumbnail" src="/images/gazoprovodi/project-2t.png" alt="Thumbnail">
+                        <img slot="image" src="/images/gazoprovodi/project-2.png" alt="Lightbox image">
+                    </Lightbox>
+                </figure>
+                </div>
+
+                <div class="slide-content proect-carousel-item">
+                    <figure class="image"> 
+                        <span class="item-content-2">
+                            <strong class="item-title">Проект 3</strong>
+                        </span>
+                    <Lightbox   noScroll="false"
+                                modalClasses = "pipu"
+                                thumbnail>
+                        <img slot="thumbnail" src="/images/gazoprovodi/project-3t.png" alt="Thumbnail">
+                        <img slot="image" src="/images/gazoprovodi/project-3.png" alt="Lightbox image">
+                    </Lightbox>
+                </figure>
+                </div>
+
+                <div class="slide-content proect-carousel-item">
+                    <figure class="image"> 
+                        <span class="item-content-2">
+                            <strong class="item-title">Проект 4</strong>
+                        </span>
+                    <Lightbox   noScroll="false"
+                                modalClasses = "pipu"
+                                thumbnail>
+                        <img slot="thumbnail" src="/images/gazoprovodi/project-4t.png" alt="Thumbnail">
+                        <img slot="image" src="/images/gazoprovodi/project-4.png" alt="Lightbox image">
+                    </Lightbox>
+                </figure>
+                </div>
+
+                <div class="slide-content proect-carousel-item">
+                    <figure class="image"> 
+                        <span class="item-content-2">
+                            <strong class="item-title">Общий вид на АГРС</strong>
+                        </span>
+                    <Lightbox   noScroll="false"
+                                modalClasses = "pipu"
+                                thumbnail>
+                        <img slot="thumbnail" src="/images/gazoprovodi/project-5t.png" alt="Thumbnail">
+                        <img slot="image" src="/images/gazoprovodi/project-5.jpg" alt="Lightbox image">
+                    </Lightbox>
+                </figure>
+                </div>
+
+                <div class="slide-content proect-carousel-item">
+                    <figure class="image"> 
+                        <span class="item-content-2">
+                            <strong class="item-title">Оборудование</strong>
+                        </span>
+                    <Lightbox   noScroll="false"
+                                modalClasses = "pipu"
+                                thumbnail>
+                        <img slot="thumbnail" src="/images/gazoprovodi/project-6t.png" alt="Thumbnail">
+                        <img slot="image" src="/images/gazoprovodi/project-6.jpg" alt="Lightbox image">
+                    </Lightbox>
+                </figure>
+                </div>
+
+                <div class="slide-content proect-carousel-item">
+                    <figure class="image"> 
+                        <span class="item-content-2">
+                            <strong class="item-title">Стартовый котлован под прокол</strong>
+                        </span>
+                    <Lightbox   noScroll="false"
+                                modalClasses = "pipu"
+                                thumbnail>
+                        <img slot="thumbnail" src="/images/gazoprovodi/project-7t.png" alt="Thumbnail">
+                        <img slot="image" src="/images/gazoprovodi/project-7.jpg" alt="Lightbox image">
+                    </Lightbox>
+                </figure>
+                </div>
+
+                <span class="control" slot="right-control">
+                            <Icon icon={faChevronRight}/>
+                </span>
+
+            </Carousel>
+        </div>
+        <div align="center" class="pt-4">
+            <a class="button is-primary is-medium is-raunded" href="/page/referens-list">Наши проекты</a>
+        </div>
+    </section>
+
+
+     <section class="section py-6 has-text-centered">
+        <div class="container text-center">
+            <div class="title titlefor">
+                <h2 class="">Этапы работы</h2>
+            </div>
+            <p class="mb-5">Алгоритм проектирования магистральных газопроводов и ГРС вместе с «Энергией Плюс»</p>
+            <div class="columns is-desktop ">
+                <div class="column logic__item svelte-rgyqg6"><img alt=""
+                                                                        class="logic__icon svelte-rgyqg6 mb-6"
+                                                                        src="/images/kotel/icons/bmk_icon_01.png">
+                    <p class="logic__text svelte-rgyqg6">Оставляете заявку / заполняете опросный лист</p>
+                </div>
+                <div class="column logic__item svelte-rgyqg6"><img alt=""
+                                                                        class="logic__icon svelte-rgyqg6 mb-6"
+                                                                        src="/images/kotel/icons/bmk_icon_04.png">
+                    <p class="logic__text svelte-rgyqg6">Получаете на рассмотрение технико-коммерческое предложение
+                        (ТКП)</p>
+                </div>
+                <div class="column logic__item svelte-rgyqg6"><img alt=""
+                                                                        class="logic__icon svelte-rgyqg6 mb-6"
+                                                                        src="/images/kotel/icons/bmk_icon_02.png">
+                    <p class="logic__text svelte-rgyqg6">Утверждаем задание на проектирование</p>
+                </div>
+            </div>
+            <div class="columns is-desktop">
+                <div class="column logic__item svelte-rgyqg6"><img alt=""
+                                                                        class="logic__icon svelte-rgyqg6 mb-6"
+                                                                        src="/images/kotel/icons/bmk_icon_09.png">
+                    <p class="logic__text svelte-rgyqg6">Заключаем договор</p>
+                </div>
+                <div class="column logic__item svelte-rgyqg6"><img alt=""
+                                                                        class="logic__icon svelte-rgyqg6 mb-6"
+                                                                        src="/images/kotel/icons/bmk_icon_06.png">
+                    <p class="logic__text svelte-rgyqg6">Утверждаем принципиальные технические решения</p>
+                </div>
+                <div class="column logic__item svelte-rgyqg6"><img alt=""
+                                                                        class="logic__icon svelte-rgyqg6 mb-6"
+                                                                        src="/images/kotel/icons/bmk_icon_03.png">
+                    <p class="logic__text svelte-rgyqg6">Разрабатываем проектную и рабочую документацию</p>
+                </div>
+            </div>
+            <div class="columns is-desktop">
+                <div class="column logic__item svelte-rgyqg6 rgyqg7"><img alt=""
+                                                                        class="logic__icon svelte-rgyqg6 mb-6"
+                                                                        src="/images/kotel/icons/bmk_icon_07.png">
+                    <p class="logic__text svelte-rgyqg6">Получаем необходимые согласования и экспертизы</p>
+                </div>
+                <div class="column logic__item svelte-rgyqg6"><img alt=""
+                                                                        class="logic__icon svelte-rgyqg6 mb-6"
+                                                                        src="/images/kotel/icons/bmk_icon_05.png">
+                    <p class="logic__text svelte-rgyqg6">Выдаём проект в производство работ</p>
+                </div>
+            </div>
+        </div>
+     </section>
 
     <section class="section vramku-columns-y">
         <div class="container">
