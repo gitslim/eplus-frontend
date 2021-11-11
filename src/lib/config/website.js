b67d53fd2e641adb9d729d9df797438cb3cc0061
Slim <DoroninEvgeniy@gmail.com>
2021-11-11 12:53:41 +0300

    Change bitrix url.

--- src/lib/config/website.js
+++ src/lib/config/website.js
@@ -9,7 +9,7 @@ const website = {
     siteUrl: `https://${domain}`,
     apiUrl: import.meta.env.VITE_API_URL || `https://api.${domain}`,
     gtmId: 'GTM-N7DB4C5',
-    bitrixHookUrl: import.meta.env.VITE_BITRIX_HOOK_URL || 'https://energy-plus.bitrix24.ru/rest/24/fur1yncokcjsejgf/',
+    bitrixHookUrl: import.meta.env.VITE_BITRIX_HOOK_URL || 'https://energy-plus.bitrix24.ru/rest/24/5r808b2ybomknquo/',
     disableAnalytics: import.meta.env.VITE_DISABLE_ANALYTICS || false
 }
 
