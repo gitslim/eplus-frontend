542a99d7d3211757b1c8853d35d0172830f7ddc7
wdr83 <91600121+wdr83@users.noreply.github.com>
2021-11-24 13:55:31 +0300

    Контакты верстка и в яндексе точка переставлена

--- src/routes/contacts.svelte
+++ src/routes/contacts.svelte
@@ -28,22 +28,49 @@
 <!--    </div>-->
 <!--</Modal>-->
 
+<style lang="scss">
+
+
+
+.titlefor:before {
+        content: "";
+        display: block;
+        position: absolute;
+        width: 100px;
+        height: 3px;
+        top: -8px;
+        left: 0;
+        background-color: #f60;
+    }
+
+</style>
+
+
 <main class="container pt-4">
     <section class="section">
         <div class="box">
-            <h1 class="title">{title}</h1>
+            <h1 class="title titlefor pt-3">{title}</h1>
             <div class="columns">
-                <div class="column is-4">
+                <div class="column">
                     <Address/>
                     <!--                    <button class="button" on:click={() => showModal.set(true)}>Заказать-->
                     <!--                        звонок-->
                     <!--                    </button>-->
+                    <span>
+                        <br/>
+                        <p class="has-text-justified">
+                            "ООО Энергия Плюс предоставляет <b>услуги по проектированию</b> опасных и особо опасных производственных объектов <b>на всей территории Российской Федерации</b> от Калининграда до Южно-Сахалинска.
+                        </p>
+                        <p class="has-text-justified">
+                            Мы предлагаем не только <b>оптимальные технические решения</b>, но и самые <b>конкурентные цены</b> на все виды услуг"
+                        </p>
+                    </span>
                     <div class="pt-6">
                         <BtnZayavka btnText="Заказать звонок"></BtnZayavka>
 <!--                        <ContactForm></ContactForm>-->
                     </div>
                 </div>
-                <div class="column is-6 is-offset-2">
+                <div class="column">
                     <YandexMap></YandexMap>
                 </div>
             </div>
