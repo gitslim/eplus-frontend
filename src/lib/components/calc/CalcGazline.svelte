e3177708f4d41840f39ebb70209d4b28871f3d21
Slim <DoroninEvgeniy@gmail.com>
2021-12-05 21:57:03 +0300

    WIP

--- src/lib/components/calc/CalcGazline.svelte
+++ src/lib/components/calc/CalcGazline.svelte
@@ -1,83 +1,79 @@
 <script>
     import {createEventDispatcher} from 'svelte'
-    // import {createForm} from 'svelte-forms-lib'
-    import Icon from 'fa-svelte'
-    import {faCheck, faExclamationTriangle, faPhone, faUser} from '@fortawesome/free-solid-svg-icons'
-    // import * as yup from 'yup'
+    import {createForm} from 'svelte-forms-lib'
     import {imask} from '@imask/svelte'
     import Switch from '$lib/components/form/Switch.svelte'
-    import {intersection} from '$lib/utils'
-    import { useForm, HintGroup, Hint, validators, email, required } from "svelte-use-form"
+
+    import Icon from 'fa-svelte'
+    import * as icons from '@fortawesome/free-solid-svg-icons'
     import ContactForm from '$lib/components/ContactForm.svelte'
-    import {jobSelected, jobTypeSelected} from '$lib/components/calc/customValidators'
 
     const dispatch = createEventDispatcher()
 
-    const form = useForm()
-
-    let jobs = []
     let lead = {}
+    let errs = {}
+    $: vals = $form
+    $: {
+        console.log('validate', vals)
+        let _errs = {}
+        if (vals.projVp || vals.montVp) {
+            if (vals.lenVp === '')
+                _errs.lenVp = 'Укажите протяженность газопровода'
+        } else if (vals.projV || vals.montV) {
+            if (vals.lenV === '')
+                _errs.lenV = 'Укажите протяженность газопровода'
+        } else {
+            _errs.common = 'Выберите вид работ'
+        }
 
-    $: isVp = intersection(jobs, ['projectVp', 'montVp']).length > 0
-    $: isV = intersection(jobs, ['projectV', 'montV']).length > 0
-    $: formValid = isVp || isV
+        if (vals.name === '')
+            _errs.name = 'Введите имя'
+        if(vals.phone === '')
+            _errs.phone = 'Введите телефон'
 
-    function handleSuccess() {
-        console.debug('success')
-        dispatch('complete', {
-        })
+        errs = _errs
+        console.log('validated', errs)
     }
+    $: valid = Object.keys(errs).length === 0
 
-    $: amount = 0
-    // $: isVp = intersection($form.jobs, ['projectVp', 'smrVp']).length > 0
-    // $: nameCls = $touched.name ? ($errors.name ? 'is-danger' : 'is-success') : ''
-    // $: phoneCls = $touched.phone ? ($errors.phone ? 'is-danger' : 'is-success') : ''
-    // $: powerVpCls = $touched.powerVp ? ($errors.powerVp ? 'is-danger' : 'is-success') : ''
+    const {form, isSubmitting, handleChange, handleSubmit} = createForm({
+        initialValues: {
+            projVp: false,
+            montVp: false,
+            lenVp: '',
+            needGrpsh: false,
 
-    function accept({detail: maskRef}) {
-        console.log('accept', maskRef.value)
-        // value = maskRef.value
-    }
+            projV: false,
+            montV: false,
+            lenV: '',
+            needGru: false,
+            needUchet: false,
 
-    function complete({detail: maskRef}) {
-        console.log('complete', maskRef.unmaskedValue)
-    }
+            name: '',
+            phone: ''
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values))
+        }
+    })
 
-    function handleJobsInput(e) {
-        console.log(e)
-        jobs = e.target.value
-    }
 </script>
 
 
 <div class="container">
-    <form use:form>
-<!--        <button on:click={()=>{jobs = 'foo'}} on:click|preventDefault>Click</button>-->
-<!--        <input name="jobs"  value={jobs2} on:input={handleJobsInput}/>-->
+    <form on:submit={handleSubmit}>
+
+        <div class="is-divider" data-content="Внутриплощадочный газопровод"></div>
+
         <div class="field is-horizontal">
             <div class="field-label is-normal">
                 <label class="label">Вид работ:</label>
             </div>
-             <div class="field-body">
-                    <div class="field">
-<!--                        <div class="control">-->
-<!--                            <label class="checkbox">-->
-<!--                                <input name="projectVp" type="checkbox"  use:validators={[jobTypeSelected]}>-->
-<!--                                проект на внутриплощадочный газопровод-->
-<!--                            </label>-->
-<!--                        </div>-->
-                        <Switch name="projectVp" value="projectVp" bind:group={jobs}>проект на внутриплощадочный газопровод</Switch>
-                    </div>
-                </div>
-        </div>
-
-        <div class="field is-horizontal">
-            <div class="field-label is-normal">
-                <label class="label"></label>
-            </div>
             <div class="field-body">
                 <div class="field">
-                    <Switch name="projectV" value="projectV" bind:group={jobs}>проект на внутренний газопровод</Switch>
+                    <Switch name="projVp" bind:value={$form.projVp} on:change={handleChange}>проект на
+                        внутриплощадочный газопровод
+                    </Switch>
                 </div>
             </div>
         </div>
@@ -88,23 +84,14 @@
             </div>
             <div class="field-body">
                 <div class="field">
-                    <Switch name="montVp" value="montVp" bind:group={jobs}>монтаж внутриплощадочного газопровода</Switch>
+                    <Switch name="montVp" bind:value={$form.montVp} on:change={handleChange}>монтаж внутриплощадочного
+                        газопровода
+                    </Switch>
                 </div>
             </div>
         </div>
 
-        <div class="field is-horizontal">
-            <div class="field-label is-normal">
-                <label class="label"></label>
-            </div>
-            <div class="field-body">
-                <div class="field">
-                    <Switch name="montV" value="montV" bind:group={jobs}>монтаж внутреннего газопровода</Switch>
-                </div>
-            </div>
-        </div>
-
-        <div class="field is-horizontal" class:is-hidden={!isVp}>
+        <div class="field is-horizontal" class:is-hidden={!($form.projVp || $form.montVp)}>
             <div class="field-label is-expanded">
                 <label class="label">Ориентировочная протяженность внутриплощадочного газопровода, м</label>
             </div>
@@ -114,54 +101,190 @@
                         <input name="lenVp"
                                use:imask={{mask: /^\d{0,5}$/}}
                                class="input"
+                               class:is-danger={errs.lenVp}
                                placeholder="Протяженность, м"
+                               bind:value={$form.lenVp}
+                               on:change={handleChange}
                         />
-                        <span class="icon is-small is-left"><Icon icon="{faUser}"/></span>
-                        <!--{#if powerVpCls === 'is-danger'}-->
-                        <!--    <div class="icon is-small is-right has-text-danger">-->
-                        <!--        <Icon icon="{faExclamationTriangle}"/>-->
-                        <!--    </div>-->
-                        <!--{:else if powerVpCls === 'is-success'}-->
-                        <!--    <div class="icon is-small is-right has-text-success">-->
-                        <!--        <Icon icon="{faCheck}"/>-->
-                        <!--    </div>-->
-                        <!--{/if}-->
+                        <span class="icon is-small is-left"><Icon icon="{icons.faBacon}"/></span>
+                        {#if errs.lenVp}
+                            <div class="icon is-small is-right has-text-danger">
+                                <Icon icon="{icons.faExclamationTriangle}"/>
+                            </div>
+                        {:else}
+                            <div class="icon is-small is-right">
+                                <Icon icon="{icons.faCheck}"/>
+                            </div>
+                        {/if}
                     </div>
-                    <!--{#if powerVpCls === 'is-danger'}-->
-                    <!--    <p class="help is-danger">{$errors.powerVp}</p>-->
-                    <!--{/if}-->
+                    {#if errs.lenVp}
+                        <p class="help is-danger">{errs.lenVp}</p>
+                    {/if}
+                </div>
+            </div>
+        </div>
+
+        <div class="field is-horizontal" class:is-hidden={!($form.projVp || $form.montVp)}>
+            <div class="field-label is-normal">
+                <label class="label"></label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <Switch name="needGrpsh" bind:value={$form.needGrpsh} on:change={handleChange}>Необходимость
+                        установки ГРПШ
+                    </Switch>
+                </div>
+            </div>
+        </div>
+
+        <div class="is-divider" data-content="Внутренний газопровод"></div>
+
+        <div class="field is-horizontal">
+            <div class="field-label is-normal">
+                <label class="label">Вид работ:</label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <Switch name="projV" bind:value={$form.projV} on:change={handleChange}>проект на внутренний
+                        газопровод
+                    </Switch>
                 </div>
             </div>
         </div>
 
         <div class="field is-horizontal">
             <div class="field-label is-normal">
+                <label class="label"></label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <Switch name="montV" bind:value={$form.montV} on:change={handleChange}>монтаж внутреннего
+                        газопровода
+                    </Switch>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="field is-horizontal" class:is-hidden={!($form.projV || $form.montV)}>
+            <div class="field-label is-expanded">
+                <label class="label">Ориентировочная протяженность внутреннего газопровода, м</label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <div class="control has-icons-left has-icons-right">
+                        <input name="lenV"
+                               use:imask={{mask: /^\d{0,5}$/}}
+                               class="input"
+                               class:is-danger={errs.lenV}
+                               placeholder="Протяженность, м"
+                               bind:value={$form.lenV}
+                               on:change={handleChange}
+                        />
+                        <span class="icon is-small is-left"><Icon icon="{icons.faBacon}"/></span>
+                        {#if errs.lenV}
+                            <div class="icon is-small is-right has-text-danger">
+                                <Icon icon="{icons.faExclamationTriangle}"/>
+                            </div>
+                        {:else}
+                            <div class="icon is-small is-right">
+                                <Icon icon="{icons.faCheck}"/>
+                            </div>
+                        {/if}
+                    </div>
+                    {#if errs.lenV}
+                        <p class="help is-danger">{errs.lenV}</p>
+                    {/if}
+                </div>
+            </div>
+        </div>
+
+        <div class="field is-horizontal" class:is-hidden={!($form.projV || $form.montV)}>
+            <div class="field-label is-normal">
+                <label class="label"></label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <Switch name="needGru" bind:value={$form.needGru} on:change={handleChange}>Необходимость установки
+                        ГРУ
+                    </Switch>
+                </div>
+            </div>
+        </div>
+
+        <div class="field is-horizontal" class:is-hidden={!($form.projV || $form.montV)}>
+            <div class="field-label is-normal">
+                <label class="label"></label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <Switch name="needUchet" bind:value={$form.needUchet} on:change={handleChange}>Необходимость
+                        установки коммерческого узла учета газа
+                    </Switch>
+                </div>
+            </div>
+        </div>
+
+        <div class="is-divider" data-content="Контактные данные"></div>
+
+        <div class="field is-horizontal">
+            <div class="field-label is-expanded">
+                <label class="label">Ваше имя:</label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <div class="control has-icons-left has-icons-right">
+                        <input name="name"
+                               class="input"
+                               class:is-danger={errs.name}
+                               bind:value={$form.name}
+                               on:change={handleChange}
+                        />
+                        <span class="icon is-small is-left"><Icon icon="{icons.faUser}"/></span>
+                        {#if errs.name}
+                            <div class="icon is-small is-right has-text-danger">
+                                <Icon icon="{icons.faExclamationTriangle}"/>
+                            </div>
+                        {:else}
+                            <div class="icon is-small is-right">
+                                <Icon icon="{icons.faCheck}"/>
+                            </div>
+                        {/if}
+                    </div>
+                    {#if errs.name}
+                        <p class="help is-danger">{errs.name}</p>
+                    {/if}
+                </div>
+            </div>
+        </div>
+
+        <div class="field is-horizontal">
+            <div class="field-label is-expanded">
                 <label class="label">Телефон:</label>
             </div>
             <div class="field-body">
                 <div class="field">
-<!--                    <ContactForm {lead} on:success={(event)=>handleSuccess(event.detail)}/>-->
                     <div class="control has-icons-left has-icons-right">
                         <input name="phone"
-                               use:imask={{mask: /^\d{0,10}$/}}
-                               use:validators={[required]}
                                class="input"
-                               placeholder="10 цифр номера"
+                               class:is-danger={errs.phone}
+                               bind:value={$form.phone}
+                               on:change={handleChange}
                         />
-                        <span class="icon is-small is-left"><Icon icon="{faUser}"/></span>
-                        <HintGroup for="phone">
-                            <Hint on="required">Введите номер телефона</Hint>
-                        </HintGroup>
-                        <!--{#if powerVpCls === 'is-danger'}-->
-                        <!--    <div class="icon is-small is-right has-text-danger">-->
-                        <!--        <Icon icon="{faExclamationTriangle}"/>-->
-                        <!--    </div>-->
-                        <!--{:else if powerVpCls === 'is-success'}-->
-                        <!--    <div class="icon is-small is-right has-text-success">-->
-                        <!--        <Icon icon="{faCheck}"/>-->
-                        <!--    </div>-->
-                        <!--{/if}-->
+                        <span class="icon is-small is-left"><Icon icon="{icons.faPhone}"/></span>
+                        {#if errs.phone}
+                            <div class="icon is-small is-right has-text-danger">
+                                <Icon icon="{icons.faExclamationTriangle}"/>
+                            </div>
+                        {:else}
+                            <div class="icon is-small is-right">
+                                <Icon icon="{icons.faCheck}"/>
+                            </div>
+                        {/if}
                     </div>
+                    {#if errs.phone}
+                        <p class="help is-danger">{errs.phone}</p>
+                    {/if}
                 </div>
             </div>
         </div>
@@ -173,18 +296,18 @@
             <div class="field-body">
                 <div class="field">
                     <div class="control">
-                        <button class="button is-primary" disabled={!formValid} on:click|preventDefault>
+                        <button class="button is-primary" disabled={!valid} on:click|preventDefault>
                             Рассчитать
                         </button>
                     </div>
                 </div>
             </div>
         </div>
+
     </form>
-    <pre>
-		{JSON.stringify($form.values, null, 1)}
-	</pre>
-    <pre>
-		{JSON.stringify($form, null, 1)}
-	</pre>
+<!--    <pre>-->
+<!--        valid: {JSON.stringify(valid, null, 1)}-->
+<!--        errs: {JSON.stringify(errs, null, 1)}-->
+<!--        vals: {JSON.stringify(vals, null, 1)}-->
+<!--	</pre>-->
 </div>
