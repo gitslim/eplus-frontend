1aec50b4c181142dbc8822bcaa6bd2e49731929f
Slim <DoroninEvgeniy@gmail.com>
2023-09-11 00:46:01 +0300

    Rel=canonical. Layout DOMContentLoaded event.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,10 +8,26 @@
     import website from '$lib/config/website'
     import BackToTop from '$lib/components/BackToTop.svelte'
     import RoistatCounter from '$lib/counters/RoistatCounter.svelte'
+    import {afterUpdate} from 'svelte'
+    import {page} from '$app/stores'
 
     const {disableAnalytics, gtmId} = website
+
+    function onRouteChange() {
+        alert('route changed')
+    }
+
+    afterUpdate(() => {
+        window.document.dispatchEvent(new Event('DOMContentLoaded', {
+            bubbles: true,
+            cancelable: true
+        }))
+    })
 </script>
 
+<svelte:head>
+    <link rel="canonical" href="{website.siteUrl}{$page.path}">
+</svelte:head>
 <style global lang="scss">
   .body-wrapper {
     padding-top: 3.25rem;
@@ -19,7 +35,7 @@
 </style>
 
 {#if !disableAnalytics}
-<GTM {gtmId}/>
+    <GTM {gtmId}/>
 {/if}
 
 <div class="body-wrapper">
