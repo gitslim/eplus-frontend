8b8b0877b258b4e6b99a277eb4c3f5825c7c5666
unknown <ebisu83d@gmail.com>
2023-09-11 13:30:39 +0300

    Fix Quize fixing price bitrix api

--- src/routes/ep/lead.json.js
+++ src/routes/ep/lead.json.js
@@ -1,19 +1,21 @@
-export const post = async request => {
-    // const bitrixUrl = import.meta.env.VITE_BITRIX_HOOK_URL
-    const bitrixUrl = process.env['VITE_BITRIX_HOOK_URL']
-    const {title, name, phone, comments} = request.body
+export const post = async (request) => {
+  //   const bitrixUrl = import.meta.env.VITE_BITRIX_HOOK_URL;
+  const bitrixUrl = process.env['VITE_BITRIX_HOOK_URL'];
+  const { title, name, phone, email, comments } = request.body;
 
-    let res = await fetch(`${bitrixUrl}crm.lead.add.json?FIELDS[TITLE]=${title}&FIELDS[NAME]=${name}&FIELDS[PHONE][0][VALUE]=${phone}&FIELDS[COMMENTS]=${comments}`)
+  let res = await fetch(
+    `${bitrixUrl}crm.lead.add.json?FIELDS[TITLE]=${title}&FIELDS[NAME]=${name}&FIELDS[PHONE][0][VALUE]=${phone}&FIELDS[EMAIL][0][VALUE]=${email}&FIELDS[COMMENTS]=${comments}`,
+  );
 
-    console.log(res)
+  console.log(res);
 
-    if (res.ok) {
-        return {
-            status: 200,
-            body: await res.json()
-        }
-    }
+  if (res.ok) {
     return {
-        status: res.status,
-    }
-}
+      status: 200,
+      body: await res.json(),
+    };
+  }
+  return {
+    status: res.status,
+  };
+};
