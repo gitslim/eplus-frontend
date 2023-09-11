aa30855e625fb31b0683eeb6b23d512b2015d4cb
Slim <DoroninEvgeniy@gmail.com>
2023-09-11 15:51:36 +0300

    Fix bitrix fetch request undefined values.

--- src/routes/ep/lead.json.js
+++ src/routes/ep/lead.json.js
@@ -1,7 +1,12 @@
 export const post = async (request) => {
   //   const bitrixUrl = import.meta.env.VITE_BITRIX_HOOK_URL;
   const bitrixUrl = process.env['VITE_BITRIX_HOOK_URL'];
-  const { title, name, phone, email, comments } = request.body;
+  let { title, name, phone, email, comments } = request.body;
+  title = title || ""
+  name = name || ""
+  phone = phone || ""
+  email = email || ""
+  comments = comments || ""
 
   let res = await fetch(
     `${bitrixUrl}crm.lead.add.json?FIELDS[TITLE]=${title}&FIELDS[NAME]=${name}&FIELDS[PHONE][0][VALUE]=${phone}&FIELDS[EMAIL][0][VALUE]=${email}&FIELDS[COMMENTS]=${comments}`,
