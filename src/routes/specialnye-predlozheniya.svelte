16e49871cf5bc89a9787c0a489c1ee382279a86d
unknown <ebisu83d@gmail.com>
2024-06-25 15:49:39 +0300

    edite page mobile view and add link konteynernaya-kotelnaya-800kW

---
+++ src/routes/specialnye-predlozheniya.svelte
@@ -0,0 +1,92 @@
+<script context="module">
+    import SvelteSeo from 'svelte-seo'
+</script>
+
+<script>
+    import CallbackForm from '$lib/components/CallbackForm.svelte'
+    import SidebarRight from '$lib/components/SidebarRight.svelte'
+
+    let title = 'Специальные предложения'
+</script>
+
+<style lang="scss">
+  .column-content {
+    padding-left: 15px;
+  }
+
+  .content__wrap {
+    display: flex;
+    @media screen and (max-width: 900px) {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+
+  ul {
+    list-style: disc;
+    li{
+      padding-bottom: 10px;
+    }
+  }
+
+  .content-block {
+    width: 65%;
+    margin-right: 10px;
+    @media screen and (max-width: 570px) {
+      width: 100%;
+      padding: 15px;
+      margin-right: 0px;
+    }
+  }
+
+  .content-bottom {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .block-sidebar {
+    margin: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    @media screen and (max-width: 570px) {
+      width: 90%;
+    }
+  }
+</style>
+
+<SvelteSeo {title}/>
+<main class="container">
+    <div class="content__wrap">
+        <div class="content-block is-max-desctop">
+            <section>
+                <div class="widget">
+                    <h2 class="title">
+                        <span><span>{title}</span> </span>
+                        <span class="heading-line"></span>
+                    </h2>
+                </div>
+                <div class="column-content">
+                    <ul>
+                        <li>
+                            <a href="/page/tehnicheskoe-perevooruzhenie-kotelnoj">Техническое
+                                перевооружение котельной</a>
+                        </li>
+                        <li>
+                            <a href="/page/vnutriploshadochnye-gazoprovody">Внутриплощадочные
+                                и внутренние газопроводы</a>
+                        </li>
+                        <li>
+                          <a href="/page/konteynernaya-kotelnaya-800kW"> Контейнерная котельная 800 кВт в наличии </a>
+                      </li>
+                    </ul>
+                </div>
+            </section>
+            <div class="content-bottom">
+                <CallbackForm left white/>
+            </div>
+        </div>
+        <div class="block-sidebar">
+            <SidebarRight/>
+        </div>
+    </div>
+</main>
